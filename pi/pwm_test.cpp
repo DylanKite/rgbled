a5f42fe784223e54cb421a7586648831972e96e1
Dylan Kite <dylan44117@gmail.com>
2020-11-05 23:49:04 -0500

    starting pwm test

---
+++ pi/pwm_test.cpp
@@ -0,0 +1,9 @@
+#include "pwm_test.h"
+
+#include <stdio.h>
+#include <stdlib>
+
+int message::test_pwm_usec(uint16_t pulse_width_usec, uint8_t n_pulses)
+{
+	
+}
---
+++ pi/pwm_test.h
@@ -0,0 +1,10 @@
+#include <cstdint>
+
+class message {
+
+
+	public: 
+	int send_message();
+	int test_pwm_usec(uint16_t pulse_width_usec, uint8_t n_pulses);
+	private: 
+} 
