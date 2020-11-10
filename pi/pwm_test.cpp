ddb157b0ed9c41b686502fd2f2a18095f954f468
Dylan Kite <dylan44117@gmail.com>
2020-11-09 19:24:51 -0500

    created the makefile

--- pi/pwm_test.cpp
+++ pi/pwm_test.cpp
@@ -1,9 +1,47 @@
 #include "pwm_test.h"
 
-#include <stdio.h>
-#include <stdlib>
 
-int message::test_pwm_usec(uint16_t pulse_width_usec, uint8_t n_pulses)
+#include <wiringPi.h>
+
+#include <cstdint>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define SELECTED_PIN 12
+
+using namespace std;
+message::message()
 {
-	
+    wiringPiSetup();
+    pinMode(SELECTED_PIN, PWM_OUTPUT);
+}
+
+message::~message()
+{
+
+}
+
+/*
+int8_t message::init_pins(uint8_t selected_pin, uint8_t pin_mode)
+{
+
+}
+*/
+int8_t message::test_pwm_usec(uint16_t pulse_width_usec, uint8_t n_pulses)
+{
+ return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc > 1 )
+    {
+        for(int i = 0; i < argc; i++)
+        {
+            cout << argv[i] << "\n";
+        }
+    }
+    return 0;
 }
--- pi/pwm_test.h
+++ pi/pwm_test.h
@@ -3,8 +3,12 @@
 class message {
 
 
-	public: 
-	int send_message();
-	int test_pwm_usec(uint16_t pulse_width_usec, uint8_t n_pulses);
-	private: 
-} 
+    public:
+        message();
+        ~message();
+
+        int8_t send_message();
+        int8_t test_pwm_usec(uint16_t pulse_width_usec, uint8_t n_pulses);
+    private:
+        int8_t init_pins(uint8_t selected_pin, uint8_t pin_mode);
+};
