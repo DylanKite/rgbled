b310b5d7e8f2cffe7ca26bb86569c63d7f610ddb
Dylan Kite <dylan44117@gmail.com>
2020-11-18 17:25:29 -0500

    changing branches since pwm is not usable

--- pi/pwm_test.cpp
+++ pi/pwm_test.cpp
@@ -9,8 +9,8 @@
 #include <stdlib.h>
 
 
-#define SELECTED_PIN 12
-
+#define SELECTED_PIN 1 // this is physical pin 12 on the board and gpio pin 18
+#define N_PULSES 100
 using namespace std;
 message::message()
 {
@@ -29,19 +29,41 @@ int8_t message::init_pins(uint8_t selected_pin, uint8_t pin_mode)
 
 }
 */
+
 int8_t message::test_pwm_usec(uint16_t pulse_width_usec, uint8_t n_pulses)
 {
- return 0;
+    pwmWrite(SELECTED_PIN, pulse_width_usec); 
+    cout << "outputting PWM";
+    return 0;
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc > 1 )
+    uint16_t freq_usec;
+/*    if (argc > 1 )
     {
         for(int i = 0; i < argc; i++)
         {
-            cout << argv[i] << "\n";
+            cout << "\n" << argv[i] << "\n i val: " << i << "\n";
         }
     }
+    else
+    {
+        cout << argc << "argc val";
+    }
+*/
+    if (argc == 2)
+    {
+        freq_usec = atoi(argv[1]);
+        cout << "freq usec selected: " << freq_usec << "\n";
+    }
+    else
+    {
+        cout << "please enter the pwm frequency\n argc = " << argc << "\n";
+        return -1;
+    }
+    
+    message pwm;
+    pwm.test_pwm_usec(freq_usec, N_PULSES);
     return 0;
 }
